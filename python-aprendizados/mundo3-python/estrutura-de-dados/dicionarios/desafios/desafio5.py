7afe24f2b276d3f21a1e0f9dad7866938745099d
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-01-18 16:04:58 -0300

    Termina o desafio5

--- python-aprendizados/mundo3-python/estrutura-de-dados/dicionarios/desafios/desafio5.py
+++ python-aprendizados/mundo3-python/estrutura-de-dados/dicionarios/desafios/desafio5.py
@@ -14,22 +14,40 @@ from statistics import mean
 ListaMulher = list()
 ListaCadastro = list()
 resposta = ''
-mediaIdade = 0
+soma = 0
+
 while True:
     Dicionario = dict()
     Dicionario['Nome'] = str(input('Nome: '))
-    Dicionario['Sexo'] = str(input('Sexo [M/F]: ')).upper()
+    
+    while True:
+        Dicionario['Sexo'] = str(input('Sexo [M/F]: ')).upper()
+        if Dicionario['Sexo'] in 'MF':
+            break
+        print('Sexo inválido! Digite "M" para masculino e "F" para feminino: ')       
+
     Dicionario['Idade'] = int(input('Idade: '))
     
+    soma += Dicionario['Idade']
+
     ListaCadastro.append(Dicionario)
 
+    
     if Dicionario['Sexo'] == 'F':
         ListaMulher.append(Dicionario)
+    
+    while True:
 
-    resposta = str(input('Quer cadastrar mais alguém?[S/N]: ')).upper()
+        resposta = str(input('Quer cadastrar mais alguém?[S/N]: ')).upper()
+        
+        if resposta in 'SN':
+            break
+        print('Resposta inválida! Digite "S" para continuar e "N" para encerrar: ')
+        
 
     if resposta == 'N':
-        break
+            break
+mediaIdade = soma / len(ListaCadastro)
 
 print(f'Foram cadastrados {len(ListaCadastro)} nomes: ',end=' ')
 for nomes in ListaCadastro:
@@ -37,6 +55,14 @@ for nomes in ListaCadastro:
 
 print()
 
+print(f'A média das idades é de {mediaIdade}')
+
 print(f'foram cadastrada(as) {len(ListaMulher)} mulheres, sendo elas: ', end=' ')
 for mulheres in ListaMulher:
-    print(mulheres['Nome'], end=', ')
+    print(mulheres['Nome'])
+
+print('Pessoas com idade acima da média:', end='')
+
+for pessoa in ListaCadastro:
+    if pessoa['Idade'] > mediaIdade:
+        print(f"Nome: {pessoa['Nome']}, Sexo: {pessoa['Sexo']}, Idade: {pessoa['Idade']}")
