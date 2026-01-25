2d9d1a5231a119fda87d93a3bf57113b3ad1b95c
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-01-25 13:54:52 -0300

    Atuliza o desafio3

--- python-aprendizados/mundo3-python/Funcoes-Modularizacao/funcoes/desafios/desafio3.py
+++ python-aprendizados/mundo3-python/Funcoes-Modularizacao/funcoes/desafios/desafio3.py
@@ -8,39 +8,49 @@ b) De 10 até 0, de 2 em 2
 c)Uma contagem personalizada
 '''
 from time import sleep
-def contador():
-    print('Contagem de  1 a 10')
-    print()
-    for valores in range(1, 11,1): 
-        print(valores, end=' ', flush=True)
-        sleep(0.5)
-    print('\n')
+def contador(inicio, fim, passo):
 
-    print('Contagem regressiva de 10 a 0, de 2 em 2:')
-    print()
-    for inverso in range(10, 0, -2):
-        print(inverso, end=' ', flush=True)
-        sleep(0.5)
+    if passo == 0:
+        print('Valor inválido!Por tanto o passo será igual a 1')
 
+        passo = 1
 
-contador()
+ 
+
+    if inicio < fim:
+
+        passo = passo
+
+        for valores in range(inicio, fim + 1, passo): 
+            print(valores, end=' ', flush=True)
+            sleep(0.5)
+        print('\n')
+
+        
+        
+
+    else:
+        passo = -passo
+        for inverso in range(inicio, fim - 1, passo):
+
+            print(inverso, end=' ', flush=True)
+            sleep(0.5)
+
+print('Contagem de 1 a 10: ')
+   
+contador(1, 10, 1)
+
+print()
+
+print('Contagem regressiva de 10 a 0, de 2 em 2: ')
+        
+contador(10, 0, 2)
 print('\n')
+
 inicio = int(input('inicio: '))
 
 fim = int(input('Fim: '))
 
 passo = int(input('Passo: '))
 
-if passo == 0:
-    print('Valor inválido!Por tanto o passo será igual a 1')
-
-    passo = 1
-else:
-    if inicio > fim:
-        passo = -passo
-
-print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
-
-for valor in range(inicio,fim,passo):
-    
-    print(valor, end=' ')
+contador(inicio, fim, passo)
