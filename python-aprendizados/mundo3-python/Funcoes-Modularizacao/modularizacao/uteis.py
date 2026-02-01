5d9859a6066846c22bd0d7fa5278b6777157bea6
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-01 14:58:42 -0300

    Cria arquivo para modularizacao

---
+++ python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/uteis.py
@@ -0,0 +1,13 @@
+def fatorial(n):
+    fat = 1
+    
+    for valor in range(1, n+1):
+        fat *= valor
+        
+    return fat
+
+def dobro(n):
+    return n * 2
+
+def triplo(n):
+    return n*3
