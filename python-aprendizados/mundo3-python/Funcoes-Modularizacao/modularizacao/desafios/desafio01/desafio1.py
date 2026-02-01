dee95da151d7fcf0748239cdce42cd840c458dea
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-01 16:01:32 -0300

    Remove as funções para usar o modulo

--- python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio01/desafio1.py
+++ python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio01/desafio1.py
@@ -5,40 +5,14 @@ Faça também um programa que importe esse módulo e use algumas dessas funçõe
 
 #Modulo moeda
 
-def aumentar(num):
-
-    while True:
-        valor = num * (1 + 10/100)
-        break
-    return valor
-
-
-def diminuir(num):
-
-    while True:
-        reajuste = num * (1 - 13/100)
-        break
-    return reajuste
-
-def dobro(num):
-
-    while True:
-        dobro = num * 2
-        break
-    return dobro
-
-def metade(num):
-
-    while True:
-        
-        metade = num / 2
-        break
-    return metade
+import moedas
 
 n = int(input('Digite um número: '))
 
-print(f'O reajute pra + do produto é de {aumentar(n)}')
+print(f'O reajute pra + do produto é de {moedas.aumentar(n)}')
 
-print(f'O dobro do produto é de {dobro(n)}')
+print(f'O reajuste pra - do produto é de {moedas.diminuir(n)}')
 
-print(f'A metade do produto é de {metade(n)}')
+print(f'O dobro do produto é de {moedas.dobro(n)}')
+
+print(f'A metade do produto é de {moedas.metade(n)}')
