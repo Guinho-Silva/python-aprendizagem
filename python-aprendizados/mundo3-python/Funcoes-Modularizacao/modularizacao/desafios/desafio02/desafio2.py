afd5c9eea95d8e8848845b16b0f39d5595146440
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-02 14:15:47 -0300

    Resolve o desafio2

--- python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio02/desafio2.py
+++ python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio02/desafio2.py
@@ -1,3 +1,16 @@
 '''
 2)Adapte o código do desafio 1, criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetário formatado
 '''
+
+import moeda
+
+n = int(input('Digite um número: '))
+
+print(f'O reajute pra + do produto é de {moeda.moeda(moeda.aumentar(n))}')
+
+
+print(f'O reajuste pra - do produto é de {moeda.moeda(moeda.diminuir(n))}')
+
+print(f'O dobro do produto é de {moeda.moeda(moeda.dobro(n))}')
+
+print(f'A metade do produto é de {moeda.moeda(moeda.metade(n))}')
