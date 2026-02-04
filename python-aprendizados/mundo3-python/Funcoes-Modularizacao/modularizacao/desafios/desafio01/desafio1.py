37712d9fdeb06fbecd76d2fc4a1bce6d2cfdeac3
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-04 14:36:56 -0300

    Corrige alguns erros de texto no codigo

--- python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio01/desafio1.py
+++ python-aprendizados/mundo3-python/Funcoes-Modularizacao/modularizacao/desafios/desafio01/desafio1.py
@@ -7,12 +7,12 @@ Faça também um programa que importe esse módulo e use algumas dessas funçõe
 
 import moedas
 
-n = int(input('Digite um número: '))
+n = float(input('Digite o preço: '))
 
-print(f'O reajute pra + do produto é de {moedas.aumentar(n)}')
+print(f'O reajute de 10% de {n} é de {moedas.aumentar(n)}')
 
-print(f'O reajuste pra - do produto é de {moedas.diminuir(n)}')
+print(f'O reajuste de 13% de desconto de {n} é de {moedas.diminuir(n)}')
 
-print(f'O dobro do produto é de {moedas.dobro(n)}')
+print(f'O dobro de {n} é de {moedas.dobro(n)}')
 
-print(f'A metade do produto é de {moedas.metade(n)}')
+print(f'A metade de {n} é de {moedas.metade(n)}')
