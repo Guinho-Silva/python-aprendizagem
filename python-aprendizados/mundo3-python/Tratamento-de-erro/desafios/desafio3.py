6a458746f07229b5cc53438992f400ee38d9cfc2
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-08 15:44:31 -0300

    Adiciona novas funcoes e melhora a funcao cadastrar

--- python-aprendizados/mundo3-python/Tratamento-de-erro/desafios/desafio3.py
+++ python-aprendizados/mundo3-python/Tratamento-de-erro/desafios/desafio3.py
@@ -3,41 +3,66 @@
 
 O sistema só vai ter 2 opções: Cadastrar uma nova pessoa e listar todas as pessoas cadastradas'''
 
+
+
+def leiaint(msg):
+
+    while True:
+        try:
+            num = int(input(msg))
+            return num
+        except:
+            print('Digite uma opção válida!')
+
+def leiaidade(msg):
+    while True:
+        try:
+            idade = int(input(msg))
+            return idade
+        except:
+            print('Digite um número valido')
+
+def linhas(tamanho = 42):
+    return '-' * tamanho
+
+def menu(msg):
+    print(linhas())
+    print(msg.center(42))
+    print(linhas())
+
+def opcoes(lista):
+    menu('MENU PRINCIPAL')
+    cont = 1
+    for valores in lista:
+        print(f'{cont}- {valores}')
+        cont +=1
+    opcao = leiaint('Digite sua opção: ')
+    return opcao
+
+
 def cadastrar():
     listaCad = list() 
     
     while True:
 
-        print('-'*30)
-        print(f'{'MENU PRINCIPAL':>20}')
-        print('-'*30)
-
-        print('''
-        1- Ver pessoas cadastradas
-        2- Cadastrar nova pessoa
-        3- Sair do Sistema
-        ''')
-        try:
-            opcao = int(input('Digite sua opção: '))
-        except:
-            print('Digite uma opção valida')
-            continue
+        opcao = opcoes(['opc1', 'opc2', 'opc3'])
 
         if opcao == 1:
-            print('-'*30)
-            print(f'{"PESSOAS CADASTRAS":>20}')
-            print('-'*30)
+            print(linhas())
+            print(menu('PESSOAS CADASTRADAS'))
+            print(linhas())
             if not listaCad:
                 print('Nenhumma pessoa cadastrada.')
         
         elif opcao == 2:
 
             nome = str(input('Nome: '))
-            listaCad.append(nome)
 
-            idade = int(input('Idade: '))
-            listaCad.append(idade)
+            idade = leiaidade('Idade: ')
+
+            listaCad.append([nome, idade])
             print('Cadastrado(a) com sucesso!')
+            
         elif opcao == 3:
             print('Saindo...')
             break
@@ -50,4 +75,6 @@ def cadastrar():
 
 cadastro = cadastrar()
 print(cadastro)
+
+
 
