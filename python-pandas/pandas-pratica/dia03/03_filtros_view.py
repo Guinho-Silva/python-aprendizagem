ee3879922127a0ab1bec9036d58a35b164e31e7d
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-13 17:40:50 -0300

    Finaliza a aula de filtros com copia

---
+++ python-pandas/pandas-pratica/dia03/03_filtros_view.py
@@ -0,0 +1,19 @@
+#%%
+
+import pandas as pd
+
+clientes = pd.read_csv("../arquivos/clientes.csv", sep=';')
+
+clientes.head()
+
+#%%
+
+filtro = clientes['qtdePontos'] == 0 
+
+clientes_0 = clientes[filtro].copy()
+
+clientes_0['flag_1'] = 1
+
+clientes_0
+#%%
+
