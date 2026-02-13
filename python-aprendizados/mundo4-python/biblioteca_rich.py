464a4c297a92cff5b63c77b2078a83b07063f2df
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-13 15:35:31 -0300

    Conhecendo a biblioteca rich

---
+++ python-aprendizados/mundo4-python/biblioteca_rich.py
@@ -0,0 +1,10 @@
+from rich import print
+"""
+https://rich.readthedocs.io/en/latest/introduction.html
+"""
+
+'''
+Terminal
+python -m rich.emoji
+'''
+print('Hello, [red]World![/] :earth_americas:')
