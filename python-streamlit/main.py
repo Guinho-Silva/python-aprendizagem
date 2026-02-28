724f1e2dea51ae86f6ba1c0b52a911692e8ac3f6
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-02-28 14:01:44 -0300

    Adiciona abas e gráficos

--- python-streamlit/main.py
+++ python-streamlit/main.py
@@ -23,8 +23,42 @@ if file_upload:
     #leitura dos dados
     df = pd.read_csv(file_upload)
 
+    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y").dt.date
+
+    exp1= st.expander("Dados Brutos")
+
     # Exibição dos dados no App
 
     columns_fat = {"Valor": st.column_config.NumberColumn("Valor", format="R$%d")}
 
-    st.dataframe(df, hide_index=True, column_config = columns_fat)
+    exp1.dataframe(df, hide_index=True, column_config = columns_fat)
+
+    #Visão instituição
+    exp2 = st.expander("Instituições")
+
+    df_instituicao = df.pivot_table(index="Data", columns = "Instituição", values="Valor")
+
+    tab_data, tab_history, tab_share = exp2.tabs(["Dados", "Histórico", "Distribuição"])
+
+
+    with tab_data:
+
+        st.dataframe(df_instituicao)
+
+    #Gráfico
+    with tab_history:
+        st.line_chart(df_instituicao)
+
+    with tab_share:
+
+        date = st.date_input("Data para Distribuição", min_value=df_instituicao.index.min(),
+        max_value=df_instituicao.index.max())
+
+        if date not in df_instituicao.index:
+            st.warning("Entre com uma data válida")
+        
+        else:
+            #Obtem a ultima data de dados
+            last_dt =  df_instituicao.sort_index().iloc[-1]
+
+            st.bar_chart(last_dt)
