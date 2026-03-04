b2450d1a0c6cb38653eaf61a38f1089a667415e5
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-03-04 14:53:05 -0300

    Adiciona a biblioteca rich e remove algumas linhas

--- python-aprendizados/mundo4-python/desafios/desafio1.py
+++ python-aprendizados/mundo4-python/desafios/desafio1.py
@@ -1,20 +1,24 @@
 '''
 1)Crie uma classe funcionário, onde podemos cadastrar nome, setor e cargo. Crie também um médoto que permita ao funcionário se apresentar
 '''
-
+from rich import print
+from rich import inspect
 
 class funcionario:
+
+    #Atributos de classe (global)
+
+    empresa = 'Curso em Video'
+
     def __init__(self, nome, setor, cargo): #Método construtor
 
-        #Atributos
+        #Atributos de instancia
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
-
-
-    def apresentar(self):
-        return f'Me chamo {self.nome} sou {self.cargo} no setor de {self.setor} da empresa Curso em Video'
+    def apresentar(self) -> str:
+        return f'Me chamo {self.nome} sou {self.cargo} no setor de {self.setor} da empresa {funcionario.empresa}'
     
 teste = funcionario('Iago', 'Dados', 'Analista de Dados')
 
